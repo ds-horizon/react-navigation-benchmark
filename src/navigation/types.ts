5dc9b67c7d4d568051910830c75dd2f00483ce1f
poonamjain96 <poonam.jain@dream11.com>
2025-05-09 15:19:10 +0530

    Deleted: unused components

--- src/navigation/types.ts
+++ src/navigation/types.ts
@@ -1,8 +1,4 @@
 export type RootStackParamList = {
-  Home: undefined;
-  Details: undefined;
-  Profile: undefined;
-  Settings: undefined;
   ScreenA: { fromScreenB?: boolean } | undefined;
   ScreenB: undefined;
 }; 
