7df4d3b6273a34e921d8b8d601f94fd51ce12119
poonamjain96 <poonam.jain@dream11.com>
2025-05-14 12:37:15 +0530

    fix: added ios app start up marker

--- ios/NavigationBenchmark/AppDelegate.swift
+++ ios/NavigationBenchmark/AppDelegate.swift
@@ -2,6 +2,7 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
+import PerformanceTracker
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
   ) -> Bool {
+    PerformanceTracker.sharedInstance()?.track(
+          "AppLaunchStart",
+          timestamp: TimeInterval(Int64(Date().timeIntervalSince1970 * 1000)),
+          meta: nil,
+          writeLogInFile: true
+        )
+    
     let delegate = ReactNativeDelegate()
     let factory = RCTReactNativeFactory(delegate: delegate)
     delegate.dependencyProvider = RCTAppDependencyProvider()
